cef57d28102b95757e7d49df28eb0f23ade6e4ae
yorliabdulai <yorliabdulai1@gmail.com>
2024-01-25 17:15:34 +0000

    Added a new route for USSD.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const ussdRoute = require("./ussdRoute")
 const app = express()
 const port = 3005
 app.listen(port, ()=> console.log(`Running on localhost:${port}`))
@@ -7,3 +8,4 @@ app.listen(port, ()=> console.log(`Running on localhost:${port}`))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
+app.use("/", ussdRoute)
