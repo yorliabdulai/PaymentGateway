5653bf21bb74b3bc1302a4a714fc2e22be897873
yorliabdulai <yorliabdulai1@gmail.com>
2024-01-25 17:10:34 +0000

    Update body-parser and raw-body dependencies.

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const bodyParser = require("body-parser")
 const app = express()
 const port = 3005
 app.listen(port, ()=> console.log(`Running on localhost:${port}`))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
