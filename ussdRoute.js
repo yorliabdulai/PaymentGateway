53dd829112f69e83b808063e109b74a67d267659
yorliabdulai <yorliabdulai1@gmail.com>
2024-01-26 15:59:36 +0000

    Created resonse object and initialized purchased ussd for security reasons

--- ussdRoute.js
+++ ussdRoute.js
@@ -1,8 +1,36 @@
 const express = require("express")
 const router = express.Router()
+let ussdCode = "*928*38#"
+let userSessionData = {}
 
-router.get("/", async(req, res)=>{
-    res.send({code: 200, data:"You are home"})
+router.get("/ussd", async(req, res)=>{
+    const {sessionID, userID, newSession, msisdn, userData, network} = req.body
+    console.log("Details from Arkesel", req.body)
+    let message= ""
+    let service= ""
+    let continueSession ="";
+
+
+    let response = {
+        sessionID: service,
+        msisdn: msisdn,
+        userID: userID,
+        message: message,
+        continueSession: continueSession,
+        network: network
+    }
+
+    service.send(response)
 })
 
 module.exports = router
+
+/*{
+    "sessionID": "2005506191900168",
+       "userID": "USSD_DOCUMENTATION",
+         "newSession": true,
+           "msisdn": "233271231234",
+             "userData": "*928*1#",
+               "network": "AIRTELTIGO"
+};
+*/
