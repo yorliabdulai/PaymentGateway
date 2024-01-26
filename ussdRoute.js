eb156cab091289b777b79a5dbd9880dbc5e873ba
yorliabdulai <yorliabdulai1@gmail.com>
2024-01-26 16:30:47 +0000

    Add whitelist check for USSD service access.

--- ussdRoute.js
+++ ussdRoute.js
@@ -2,6 +2,8 @@ const express = require("express")
 const router = express.Router()
 let ussdCode = "*928*38#"
 let userSessionData = {}
+const whitelist = ["233557206667"]
+let service = ""
 
 router.get("/ussd", async(req, res)=>{
     const {sessionID, userID, newSession, msisdn, userData, network} = req.body
@@ -22,6 +24,28 @@ router.get("/ussd", async(req, res)=>{
         message += "1. Pay your GMSA dues\n";
         message += "2. Pay for an event\n";
         continueSession = true;
+    }else if(newSession== false && userSessionData[sessionID].step == 1){
+        userSessionData[sessionID].service = userData;
+        if(userSessionData[sessionID].service =="1" && whitelist.includes(msisdn)){
+            service = "1"
+            message = "Please enter member's number for dues"
+            continueSession = true;
+            userSessionData[sessionID].step = userSessionData[sessionID].step + 1;
+        }else{
+            service ="1"
+            message = "You are not authorized to use this service"
+            continueSession = false;
+        }
+        if(userSessionData[sessionID].service =="2" && whitelist.includes(msisdn)){
+            service = "2"
+            message = "Please enter member's number for event"
+            continueSession = true;
+            userSessionData[sessionID].step = userSessionData[sessionID].step + 1;
+        }else{
+            service ="2"
+            message = "You are not authorized to use this service"
+            continueSession = false;
+        }
     }
 
     let response = {
